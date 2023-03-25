484767f3ffbffccdde63955a9d0588552d61b1d4
csanadm <38218165+csanadm@users.noreply.github.com>
2023-03-25 15:29:33 +0100

    Add files via upload

--- fit.py
+++ fit.py
@@ -4,18 +4,24 @@ import numpy as np
 from lmfit import minimize, Parameters, Parameter, fit_report, report_fit
 
 def mychi2fun(params, df, columns):
-    values = np.array(list(params.valuesdict().values()))
-    residuals = df.apply(lambda row: (np.dot(values, np.nan_to_num(row[columns],nan=0)) - row['Symm1'])**2, axis=1)
-    return residuals
+  values = np.array(list(params.valuesdict().values()))
+  #residuals = df.apply(lambda row: (np.dot(values, np.nan_to_num(row[columns],nan=0)) - np.nan_to_num(row['Symm1'],nan=0))**2, axis=1)
+  residuals = []
+  for index, row in df.iterrows():
+    calc = np.dot(values, row[columns].values)
+    meas = row['Symm1']
+    resid = (calc-meas)**2
+    if(np.isnan(resid)): resid=0
+    residuals.append(resid)
+  residuals = pd.Series(residuals)
+  return residuals
   
 
 df = pd.read_excel("Baseline_ALL_202303.xlsx", sheet_name="ALL")
+df['V1Min'].fillna(0, inplace=True) # Replace empty V1min values with 0
+df['V1Max'].fillna(df['V1Min'], inplace=True) # Replace empty V1Max values with V1Min
 
-#columns = df.columns
-#print(columns)
-#yvector = df["Symm1"]
-#Nrows = len(yvector)
-mycolumns = ["Duration [min]", "Humidity [%]", "Air pressure [mb]", "Water temp. [⁰C]", "Air temp. [⁰C]", "Moon illumination", "V1Min", "V1Max", "V2Min", "V2Max", "V3Min", "V3Max"]
+mycolumns = ["Duration [min]", "Humidity [%]", "Air pressure [mb]", "Water temp. [⁰C]", "Air temp. [⁰C]", "Moon illumination", "V1Min", "V1Max"]
 Ncolumns = len(mycolumns)
 
 params = Parameters()
@@ -37,10 +43,27 @@ print("NDF = %i - %i = %i" % (result.ndata,result.nvarys,result.ndata-result.nva
 for name in params.keys():
   print("%s = %f +- %f"       % (name, float(result.params[name].value), float(result.params[name].stderr)))
 
+calcs = []
+meass = []
 for index, row in df.iterrows():
+  values = np.array(list(result.params.valuesdict().values()))
+  calc = np.dot(values, row[mycolumns].values)
   meas = row['Symm1']
-  calc = np.dot(np.array(list(params.valuesdict().values())), row[mycolumns])
-  resid = meas - calc
+  resid = calc-meas
   chi2 = resid**2
-  print("%i:   %.2f  ---   %i    (resid = %.2f, chi2 = %.2f)" % (index, calc, meas, resid, chi2))
+  calcs.append(calc)
+  meass.append(meas)
+  #print("%i:   %.2f  ---   %i    (resid = %.2f, chi2 = %.2f)" % (index, calc, meas, resid, chi2))
+  
+plt.figure()
+plt.title("Linear regression fit quality check")
+plt.ylabel("Symmetry-fold")
+plt.xlabel("Measurement #")
+plt.plot(meass, marker='P', linestyle='none', label='Measured value')
+plt.plot(calcs, marker='o', linestyle='none', label='Regression calculation')
+plt.legend(loc='upper left')
+ax = plt.subplot(111)
+box = ax.get_position()
+ax.set_position([box.x0-box.width*0.05,box.y0,box.width*1.15,box.height*1.05])
+plt.savefig("fit_residuals.png")
   
