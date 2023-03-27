b84f7651d01f051b4ceac77558005b94ac75289c
csanadm <38218165+csanadm@users.noreply.github.com>
2023-03-27 11:03:51 +0200

    Add files via upload

--- amp_plots.py
+++ amp_plots.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from scipy.stats import linregress
 import numpy as np
 
 markers = ["o", "*", "x", "+", "s", "d", "|", "1", "2"] #, "_"] #NOT 10 MARKES AS THERE ARE ALSO 10 COLORS
@@ -14,28 +15,39 @@ plt.title("Amplitude vs frequency")
 plt.xlim(40,230)
 plt.ylim(0.1,4.5)
 
-xvector = df["Frequency [Hz]"].to_numpy()
+xvector0 = df["Frequency [Hz]"].to_numpy()
 
 yminvec1 = df["V1Min"].to_numpy()
 ymaxvec1 = df["V1Max"].to_numpy()
 nan_mask1 = np.isnan(ymaxvec1) # OTHER POSSIBILITY: ymaxvec = np.nan_to_num(ymaxvec, nan=-999)
-ymaxvec1[nan_mask1] = yminvec1[nan_mask1] # REPLACE MAX WITH MIN IF MAX=NAN
+ymaxvec1[nan_mask1] = yminvec1[nan_mask1] # REPLACE MAX WITH MIN IF VMAX=NAN
+xvector0 = xvector0[~np.isnan(yminvec1)] # REMOVE POINT WHERE VMIN = NAN
+ymaxvec1 = ymaxvec1[~np.isnan(yminvec1)] # REMOVE POINT WHERE VMIN = NAN
+yminvec1 = yminvec1[~np.isnan(yminvec1)] # REMOVE POINT WHERE VMIN = NAN
+
+slope, intercept, r_value, p_value, std_err = linregress(xvector0, yminvec1)
+print(f"V1Min vs frequency linear regression slope: {slope}, intercept: {intercept}, R-value: {r_value}, P-value: {p_value}, error: {std_err}")
+
+slope, intercept, r_value, p_value, std_err = linregress(xvector0, ymaxvec1)
+print(f"V1Max vs frequency linear regression slope: {slope}, intercept: {intercept}, R-value: {r_value}, P-value: {p_value}, error: {std_err}")
 
 yminvec2 = df["V2Min"].to_numpy()
 ymaxvec2 = df["V2Max"].to_numpy()
+yminvec2 = np.nan_to_num(yminvec2)
 nan_mask2 = np.isnan(ymaxvec2)
 ymaxvec2[nan_mask2] = yminvec2[nan_mask2]
 
 yminvec3 = df["V3Min"].to_numpy()
 ymaxvec3 = df["V3Max"].to_numpy()
+yminvec3 = np.nan_to_num(yminvec3)
 nan_mask3 = np.isnan(ymaxvec3)
 ymaxvec3[nan_mask3] = yminvec3[nan_mask3]
 
-for ipoint in range(len(xvector)):
-  #plt.vlines(xvector[ipoint], yminvec[ipoint], ymaxvec[ipoint], colors='r', linewidth=4)
-  if(not np.isnan(yminvec1[ipoint])): plt.gca().add_patch(Rectangle((xvector[ipoint]-1,yminvec1[ipoint]), 2, ymaxvec1[ipoint]-yminvec1[ipoint], linewidth=1, edgecolor='r', facecolor='none'))
-  if(not np.isnan(yminvec2[ipoint])): plt.gca().add_patch(Rectangle((xvector[ipoint]-1,yminvec2[ipoint]), 2, ymaxvec2[ipoint]-yminvec2[ipoint], linewidth=1, edgecolor='g', facecolor='none'))
-  if(not np.isnan(yminvec3[ipoint])): plt.gca().add_patch(Rectangle((xvector[ipoint]-1,yminvec3[ipoint]), 2, ymaxvec3[ipoint]-yminvec3[ipoint], linewidth=1, edgecolor='b', facecolor='none'))
+for ipoint in range(len(xvector0)):
+  #plt.vlines(xvector0[ipoint], yminvec1[ipoint], ymaxvec1[ipoint], colors='r', linewidth=4)
+  if(not np.isnan(yminvec1[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec1[ipoint]), 2, ymaxvec1[ipoint]-yminvec1[ipoint], linewidth=1, edgecolor='r', facecolor='none'))
+  if(not np.isnan(yminvec2[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec2[ipoint]), 2, ymaxvec2[ipoint]-yminvec2[ipoint], linewidth=1, edgecolor='g', facecolor='none'))
+  if(not np.isnan(yminvec3[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec3[ipoint]), 2, ymaxvec3[ipoint]-yminvec3[ipoint], linewidth=1, edgecolor='b', facecolor='none'))
 
 plt.gca().add_patch(Rectangle((55,3.5), 2, 0.2, linewidth=1, edgecolor='r', facecolor='none'))
 plt.gca().add_patch(Rectangle((55,3.2), 2, 0.2, linewidth=1, edgecolor='g', facecolor='none'))
