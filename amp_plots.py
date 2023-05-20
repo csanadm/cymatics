70c8018a1b5b503dfa8a850b34367ba41e0d2a7b
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-05-20 20:06:39 +0200

    Two types of amp vs freq plots

--- amp_plots.py
+++ amp_plots.py
@@ -6,17 +6,10 @@ import numpy as np
 
 markers = ["o", "*", "x", "+", "s", "d", "|", "1", "2"] #, "_"] #NOT 10 MARKES AS THERE ARE ALSO 10 COLORS
 
-df = pd.read_excel("Baseline_ALL_202303.xlsx", sheet_name="ALL")
-#df = pd.read_excel("combined_data.xlsx", sheet_name="Munka1")
+#df = pd.read_excel("Baseline_ALL_202303.xlsx", sheet_name="ALL")
+df = pd.read_excel("combined_data.xlsx", sheet_name="Munka1")
 #df = pd.read_excel("cymatics_ez_water_experiment_ALL.xlsx", sheet_name="Sheet1")
 df = df[~df['Symm1'].isna()]
-
-plt.figure()
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("Amplitude")
-plt.title("Amplitude vs frequency")
-plt.xlim(40,230)
-plt.ylim(0.1,4.5)
 
 xvector0 = df["Frequency [Hz]"].to_numpy()
 
@@ -38,28 +31,45 @@ yminvec2 = df["V2Min"].to_numpy()
 ymaxvec2 = df["V2Max"].to_numpy()
 yminvec2 = np.nan_to_num(yminvec2)
 nan_mask2 = np.isnan(ymaxvec2)
-ymaxvec2[nan_mask2] = yminvec2[nan_mask2]
+ymaxvec2[nan_mask2] = yminvec2[nan_mask2] # REPLACE MAX WITH MIN IF VMAX=NAN
 
 yminvec3 = df["V3Min"].to_numpy()
 ymaxvec3 = df["V3Max"].to_numpy()
 yminvec3 = np.nan_to_num(yminvec3)
 nan_mask3 = np.isnan(ymaxvec3)
-ymaxvec3[nan_mask3] = yminvec3[nan_mask3]
+ymaxvec3[nan_mask3] = yminvec3[nan_mask3] # REPLACE MAX WITH MIN IF VMAX=NAN
 
+plt.figure()
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Amplitude")
+plt.title("Amplitude vs frequency")
+plt.xlim(40,230)
+plt.ylim(0.1,4.5)
+for ipoint in range(len(xvector0)):
+  #plt.vlines(xvector0[ipoint], yminvec1[ipoint], ymaxvec1[ipoint], colors='r', linewidth=4)
+  if(not np.isnan(yminvec1[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec1[ipoint]), 2, ymaxvec1[ipoint]-yminvec1[ipoint], linewidth=1, edgecolor='b', facecolor='none'))
+  if(not np.isnan(yminvec2[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec2[ipoint]), 2, ymaxvec2[ipoint]-yminvec2[ipoint], linewidth=1, edgecolor='b', facecolor='none'))
+  if(not np.isnan(yminvec3[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec3[ipoint]), 2, ymaxvec3[ipoint]-yminvec3[ipoint], linewidth=1, edgecolor='b', facecolor='none'))
+plt.savefig("amplitude_vs_frequency.png")
+
+plt.figure()
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Amplitude")
+plt.title("Amplitude vs frequency")
+plt.xlim(40,230)
+plt.ylim(0.1,4.5)
 for ipoint in range(len(xvector0)):
   #plt.vlines(xvector0[ipoint], yminvec1[ipoint], ymaxvec1[ipoint], colors='r', linewidth=4)
   if(not np.isnan(yminvec1[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec1[ipoint]), 2, ymaxvec1[ipoint]-yminvec1[ipoint], linewidth=1, edgecolor='r', facecolor='none'))
   if(not np.isnan(yminvec2[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec2[ipoint]), 2, ymaxvec2[ipoint]-yminvec2[ipoint], linewidth=1, edgecolor='g', facecolor='none'))
   if(not np.isnan(yminvec3[ipoint])): plt.gca().add_patch(Rectangle((xvector0[ipoint]-1,yminvec3[ipoint]), 2, ymaxvec3[ipoint]-yminvec3[ipoint], linewidth=1, edgecolor='b', facecolor='none'))
-
 plt.gca().add_patch(Rectangle((55,3.5), 2, 0.2, linewidth=1, edgecolor='r', facecolor='none'))
 plt.gca().add_patch(Rectangle((55,3.2), 2, 0.2, linewidth=1, edgecolor='g', facecolor='none'))
 plt.gca().add_patch(Rectangle((55,2.9), 2, 0.2, linewidth=1, edgecolor='b', facecolor='none'))
 plt.text(60, 3.55, 'V1 range')
 plt.text(60, 3.25, 'V2 range')
 plt.text(60, 2.95, 'V3 range')
-
-plt.savefig("amplitude_vs_frequency.png")
+plt.savefig("amplitude_vs_frequency_threecolors.png")
 
 Symm1vals = sorted(df["Symm1"].unique())
 Nsymmvals = len(Symm1vals)
