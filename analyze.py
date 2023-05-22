0dc22a6a083317ae4283e601b1a16be2d972cef1
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-05-22 10:55:01 +0200

    Separate calculation for input and amp. change

--- analyze.py
+++ analyze.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 markers = ["o", "*", "x", "+", "s", "d", "|", "1", "2"] #, "_"] #NOT 10 MARKES AS THERE ARE ALSO 10 COLORS
 
@@ -26,6 +27,18 @@ def linear_regression_calc(xvector, yvector):
 df = pd.read_excel("combined_data.xlsx", sheet_name="Munka1")
 #df = pd.read_excel("cymatics_ez_water_experiment_ALL.xlsx", sheet_name="Sheet1")
 
+NUMBER_OF_MIN_POINTS_FOR_REGRESSION = 2
+
+filter = ""
+if(len(sys.argv)>1): filter = sys.argv[1]
+# Filter data before everything on input type or amplitude change
+if(filter=="computer" or filter=="analogue"):
+  input = filter
+  df = df[df["Audio input"]==input]
+if(filter=="manual" or filter=="automated"):
+  ampchange = filter
+  df = df[df["Amplitude change"]==ampchange]
+
 columns = df.columns
 print(columns)
 mycolumns = ["Duration [min]", "Humidity [%]", "Air pressure [mb]", "Water temp. [⁰C]", "Air temp. [⁰C]", "Moon illumination", "V1Min", "V1Max", "V2Min", "V2Max", "V3Min", "V3Max"]
@@ -46,7 +59,7 @@ for field in mycolumns:
     xvector = df_filtered[field].to_numpy()
     yvector = df_filtered["Symm1"].to_numpy()
     plt.plot(xvector, yvector, marker=markers[ifreq % len(markers)], linestyle='None', label='f='+str(f)+' Hz')
-    if(len(xvector[xvector>0])>3):
+    if(len(xvector[xvector>0])>NUMBER_OF_MIN_POINTS_FOR_REGRESSION):
       linregrpars = linear_regression_calc(xvector,yvector)
       #print("f = " + str(f) + " Hz -> " + "{:.3f}".format(linregrpars[2]))
       rvalues[ifield][ifreq] = linregrpars[2]
@@ -61,12 +74,12 @@ for field in mycolumns:
     shortfield += "_" + field.split(' ')[1]
     shortfield = shortfield.replace('.','')
   plt.ylabel("Symmetry-fold")
-  plt.title("Symmetry-fold vs " + titlefield + " and Frequency")
+  plt.title("Symmetry-fold vs " + titlefield + " for each frequency")
   ax = plt.subplot(111)
   box = ax.get_position()
   ax.set_position([box.x0-box.width*0.05,box.y0,box.width*0.92,box.height*1.06])
   ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-  plt.savefig(shortfield + ".png")
+  #plt.savefig(shortfield + ".png")
   ifield += 1
 
 plt.figure()
@@ -79,20 +92,24 @@ ax = plt.subplot(111)
 box = ax.get_position()
 ax.set_position([box.x0-box.width*0.05,box.y0,box.width*0.80,box.height*1.06])
 ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig("rvalues.png")
+#plt.savefig("rvalues.png")
 
 plt.figure()
 plt.title("Average regression r-value for symm-fold versus variable")
 plt.xlabel("")
 #plt.ylim(-0.01,0.2)
 averages = np.average(rvalues, axis=1)
+print(len(mycolumns))
+print(mycolumns)
+print(len(averages))
+print(averages)
 plt.plot(mycolumns, averages, marker=markers[0], linestyle='None')
 plt.axhline(y=0, color='black', linewidth=0.5, linestyle='--')
 plt.xticks(rotation=45, ha='right')
 ax = plt.subplot(111)
 box = ax.get_position()
 ax.set_position([box.x0-box.width*0.04,box.y0+box.height*0.17,box.width*1.15,box.height*0.90])
-plt.savefig("rvalues_averages.png")
+#plt.savefig("rvalues_averages.png")
 
 symmvals = sorted(df["Symm1"].unique())
 Nsymmvals = len(symmvals)
@@ -109,7 +126,7 @@ for field in mycolumns:
     xvector = df_filtered[field].to_numpy()
     yvector = df_filtered["Frequency [Hz]"].to_numpy()
     plt.plot(xvector, yvector, marker=markers[isymm % len(markers)], linestyle='None', label='symm='+str(symm))
-    if(len(xvector[xvector>0])>3):
+    if(len(xvector[xvector>0])>NUMBER_OF_MIN_POINTS_FOR_REGRESSION):
       linregrpars = linear_regression_calc(xvector,yvector)
       #print("f = " + str(f) + " Hz -> " + "{:.3f}".format(linregrpars[2]))
       rvalues2[ifield][isymm] = linregrpars[2]
@@ -129,7 +146,7 @@ for field in mycolumns:
   box = ax.get_position()
   ax.set_position([box.x0-box.width*0.05,box.y0,box.width*0.92,box.height*1.06])
   ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-  plt.savefig(shortfield + "_symmplot.png")
+  #plt.savefig(shortfield + "_symmplot.png")
   ifield += 1
 
 plt.figure()
@@ -142,7 +159,7 @@ ax = plt.subplot(111)
 box = ax.get_position()
 ax.set_position([box.x0-box.width*0.05,box.y0,box.width*0.80,box.height*1.06])
 ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
-plt.savefig("rvalues_symm.png")
+#plt.savefig("rvalues_symm.png")
 
 plt.figure()
 plt.title("Average regression r-value for frequency versus variable")
@@ -159,4 +176,4 @@ plt.xticks(rotation=45, ha='right')
 ax = plt.subplot(111)
 box = ax.get_position()
 ax.set_position([box.x0-box.width*0.04,box.y0+box.height*0.17,box.width*1.15,box.height*0.90])
-plt.savefig("rvalues_symm_averages.png")
+#plt.savefig("rvalues_symm_averages.png")
