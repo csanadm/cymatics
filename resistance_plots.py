7b7285c60ffd42ae749dd0c2acdd86c32d0e1a0d
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2023-05-23 22:27:08 +0200

    Single values added, plots functionized

--- resistance_plots.py
+++ resistance_plots.py
@@ -6,176 +6,78 @@ import numpy as np
 
 markers = ["o", "*", "^", "v", "s", "d", ">", "<", "h"] #, "_"] #NOT 10 MARKES AS THERE ARE ALSO 10 COLORS
 
-df = pd.read_excel("cymatics_ez_water_experiment_ALL.xlsx", sheet_name="Sheet1")
+def linear_regression_calc(xvector, yvector):
+  npoints = np.size(xvector)
+  mean_x = np.mean(xvector)
+  mean_y = np.mean(yvector)
+  SS_yy = np.sum(yvector*yvector) - npoints*mean_y*mean_y
+  SS_xy = np.sum(yvector*xvector) - npoints*mean_x*mean_y
+  SS_xx = np.sum(xvector*xvector) - npoints*mean_x*mean_x
+  if(SS_xx==0 or SS_yy==0 or npoints<=2): return (mean_y, 0, -0.999) #No meaningful r-value, don't care about b0 and b1 either
+  b1 = SS_xy/SS_xx
+  b0 = mean_y - b1*mean_x
+  resvector = yvector - b0 - b1*xvector
+  SS_res = np.sum(resvector*resvector)
+  rvalue = 1 - SS_res/SS_yy
+  #if(rvalue>0.5):
+  #  print(xvector)
+  #  print(yvector)
+  return (b0, b1, rvalue)
 
-#######################################################
-############# VERSUS SYMMETRY-FOLD ####################
-#######################################################
+def one_resinstance_diff_plot(xlabel, ylabel, title, groupby, shortgroup, groupunit, R1name, R2name, xvariable, plotname):
+  ival = 0
+  plt.figure()
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.title(title)
+  for groupvar in df[groupby].unique():
+    df_filtered = df[df[groupby]==groupvar]
+    R = df_filtered[R1name].to_numpy()
+    if(R2name != ""): R -= df_filtered[R2name].to_numpy()
+    xvar_filtered = df_filtered[xvariable]
+    plt.plot(R, xvar_filtered, marker=markers[ival % len(markers)], linestyle='None', label=(shortgroup + "=" + str(groupvar) + groupunit))
+    print(str(groupvar) + " -> " + str(linear_regression_calc(R, xvar_filtered)[2]))
+    ival += 1
+  plt.legend()
+  ax = plt.subplot(111)
+  box = ax.get_position()
+  ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
+  ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+  plt.savefig(plotname)
+
+df = pd.read_excel("cymatics_ez_water_experiment_ALL.xlsx", sheet_name="Sheet1")
+df = df[~df['Symm1'].isna()] # Remove lines with empty Symm1
+
+#####################################################################
+############### DIFFERENCES VERSUS SYMMETRY-FOLD ####################
+#####################################################################
 
 df = df.sort_values(by=["Frequency [Hz]"])
 
-ival = 0
-plt.figure()
-plt.xlabel("Resistance before, SZ - EZ diff. [MΩ]")
-plt.ylabel("Symmetry fold")
-plt.title("Symm-fold versus resistance difference")
-for freq in df["Frequency [Hz]"].unique():
-  print(f"Working on f={freq}...")
-  df_filtered = df[df["Frequency [Hz]"]==freq]
-  R1 = df_filtered["Res. before SZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. before EZ [MΩ]"].to_numpy()
-  symm_filt = df_filtered["Symm1"]
-  plt.plot(R1-R2, symm_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"f={freq} Hz")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("RBefSZEZ.png")
+one_resinstance_diff_plot("Resistance before, SZ - EZ diff. [MΩ]",  "Symmetry fold", "Symm-fold versus resistance difference", "Frequency [Hz]", "f", " Hz", "Res. before SZ [MΩ]", "Res. before EZ [MΩ]", "Symm1", "RBefSZEZ.png")
+one_resinstance_diff_plot("Resistance after, SZ - EZ diff. [MΩ]",   "Symmetry fold", "Symm-fold versus resistance difference", "Frequency [Hz]", "f", " Hz", "Res. after SZ [MΩ]",  "Res. after EZ [MΩ]",  "Symm1", "RAftSZEZ.png")
+one_resinstance_diff_plot("EZ resistance after - before diff. [MΩ]","Symmetry fold", "Symm-fold versus resistance difference", "Frequency [Hz]", "f", " Hz", "Res. after EZ [MΩ]",  "Res. before EZ [MΩ]", "Symm1", "REZAftBef.png")
+one_resinstance_diff_plot("SZ resistance after - before diff. [MΩ]","Symmetry fold", "Symm-fold versus resistance difference", "Frequency [Hz]", "f", " Hz", "Res. after SZ [MΩ]",  "Res. before SZ [MΩ]", "Symm1", "RSZAftBef.png")
 
-ival = 0
-plt.figure()
-plt.xlabel("Resistance after, SZ - EZ diff. [MΩ]")
-plt.ylabel("Symmetry fold")
-plt.title("Symm-fold versus resistance difference")
-for freq in df["Frequency [Hz]"].unique():
-  print(f"Working on f={freq}...")
-  df_filtered = df[df["Frequency [Hz]"]==freq]
-  R1 = df_filtered["Res. after SZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. after EZ [MΩ]"].to_numpy()
-  symm_filt = df_filtered["Symm1"]
-  plt.plot(R1-R2, symm_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"f={freq} Hz")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("RAftSZEZ.png")
 
-ival = 0
-plt.figure()
-plt.xlabel("EZ resistance after - before diff. [MΩ]")
-plt.ylabel("Symmetry fold")
-plt.title("Symm-fold versus resistance difference")
-for freq in df["Frequency [Hz]"].unique():
-  print(f"Working on f={freq}...")
-  df_filtered = df[df["Frequency [Hz]"]==freq]
-  R1 = df_filtered["Res. after EZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. before EZ [MΩ]"].to_numpy()
-  symm_filt = df_filtered["Symm1"]
-  plt.plot(R1-R2, symm_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"f={freq} Hz")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("REZAftBef.png")
+#####################################################################
+############### SINGLE VALUES VERSUS SYMMETRY-FOLD ##################
+#####################################################################
 
-ival = 0
-plt.figure()
-plt.xlabel("SZ resistance after - before diff. [MΩ]")
-plt.ylabel("Symmetry fold")
-plt.title("Symm-fold versus resistance difference")
-for freq in df["Frequency [Hz]"].unique():
-  print(f"Working on f={freq}...")
-  df_filtered = df[df["Frequency [Hz]"]==freq]
-  R1 = df_filtered["Res. after SZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. before SZ [MΩ]"].to_numpy()
-  symm_filt = df_filtered["Symm1"]
-  plt.plot(R1-R2, symm_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"f={freq} Hz")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("RSZAftBef.png")
+df = df.sort_values(by=["Frequency [Hz]"])
 
-#######################################################
-############# VERSUS FREQUENCY ####################
-#######################################################
+one_resinstance_diff_plot("Resistance before, SZ [MΩ]", "Symmetry fold", "Symm-fold versus resistance", "Frequency [Hz]", "f", " Hz", "Res. before SZ [MΩ]", "", "Symm1", "RBefSZ.png")
+one_resinstance_diff_plot("Resistance after, SZ [MΩ]",  "Symmetry fold", "Symm-fold versus resistance", "Frequency [Hz]", "f", " Hz", "Res. after SZ [MΩ]",  "", "Symm1", "RAftSZ.png")
+one_resinstance_diff_plot("Resistance before, EZ [MΩ]", "Symmetry fold", "Symm-fold versus resistance", "Frequency [Hz]", "f", " Hz", "Res. before EZ [MΩ]", "", "Symm1", "RBefEZ.png")
+one_resinstance_diff_plot("Resistance after, EZ [MΩ]",  "Symmetry fold", "Symm-fold versus resistance", "Frequency [Hz]", "f", " Hz", "Res. after EZ [MΩ]",  "", "Symm1", "RAftEZ.png")
+
+#####################################################################
+################## DIFFERENCES VERSUS FREQUENCY #####################
+#####################################################################
 
 df = df.sort_values(by=["Symm1"])
 
-ival = 0
-plt.figure()
-plt.xlabel("Resistance before, SZ - EZ diff. [MΩ]")
-plt.ylabel("Frequency [Hz]")
-plt.title("Frequency versus resistance difference")
-for symm in df["Symm1"].unique():
-  print(f"Working on symm={symm}...")
-  df_filtered = df[df["Symm1"]==symm]
-  R1 = df_filtered["Res. before SZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. before EZ [MΩ]"].to_numpy()
-  freq_filt = df_filtered["Frequency [Hz]"]
-  plt.plot(R1-R2, freq_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"symm={symm}")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("RBefSZEZ_vs_freq.png")
-
-ival = 0
-plt.figure()
-plt.xlabel("Resistance after, SZ - EZ diff. [MΩ]")
-plt.ylabel("Frequency [Hz]")
-plt.title("Frequency versus resistance difference")
-for symm in df["Symm1"].unique():
-  print(f"Working on symm={symm}...")
-  df_filtered = df[df["Symm1"]==symm]
-  R1 = df_filtered["Res. after SZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. after EZ [MΩ]"].to_numpy()
-  freq_filt = df_filtered["Frequency [Hz]"]
-  plt.plot(R1-R2, freq_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"symm={symm}")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("RAftSZEZ_vs_freq.png")
-
-ival = 0
-plt.figure()
-plt.xlabel("EZ resistance after - before diff. [MΩ]")
-plt.ylabel("Frequency [Hz]")
-plt.title("Frequency versus resistance difference")
-for symm in df["Symm1"].unique():
-  print(f"Working on symm={symm}...")
-  df_filtered = df[df["Symm1"]==symm]
-  R1 = df_filtered["Res. after EZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. before EZ [MΩ]"].to_numpy()
-  freq_filt = df_filtered["Frequency [Hz]"]
-  plt.plot(R1-R2, freq_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"symm={symm}")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("REZAftBef_vs_freq.png")
-
-ival = 0
-plt.figure()
-plt.xlabel("SZ resistance after - before diff. [MΩ]")
-plt.ylabel("Frequency [Hz]")
-plt.title("Frequency versus resistance difference")
-for symm in df["Symm1"].unique():
-  print(f"Working on symm={symm}...")
-  df_filtered = df[df["Symm1"]==symm]
-  R1 = df_filtered["Res. after SZ [MΩ]"].to_numpy()
-  R2 = df_filtered["Res. before SZ [MΩ]"].to_numpy()
-  freq_filt = df_filtered["Frequency [Hz]"]
-  plt.plot(R1-R2, freq_filt, marker=markers[ival % len(markers)], linestyle='None', label=f"symm={symm}")
-  ival += 1
-plt.legend()
-ax = plt.subplot(111)
-box = ax.get_position()
-ax.set_position([box.x0-box.width*0.03,box.y0,box.width*0.91,box.height*1.06])
-ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.savefig("RSZAftBef_vs_freq.png")
+one_resinstance_diff_plot("Resistance before, SZ - EZ diff. [MΩ]",  "Frequency [Hz]", "Frequency versus resistance difference", "Symm1", "symm", "", "Res. before SZ [MΩ]", "Res. before EZ [MΩ]", "Frequency [Hz]", "RBefSZEZ_vs_f.png")
+one_resinstance_diff_plot("Resistance after, SZ - EZ diff. [MΩ]",   "Frequency [Hz]", "Frequency versus resistance difference", "Symm1", "symm", "", "Res. after SZ [MΩ]",  "Res. after EZ [MΩ]",  "Frequency [Hz]", "RAftSZEZ_vs_f.png")
+one_resinstance_diff_plot("EZ resistance after - before diff. [MΩ]","Frequency [Hz]", "Frequency versus resistance difference", "Symm1", "symm", "", "Res. after EZ [MΩ]",  "Res. before EZ [MΩ]", "Frequency [Hz]", "REZAftBef_vs_f.png")
+one_resinstance_diff_plot("SZ resistance after - before diff. [MΩ]","Frequency [Hz]", "Frequency versus resistance difference", "Symm1", "symm", "", "Res. after SZ [MΩ]",  "Res. before SZ [MΩ]", "Frequency [Hz]", "RSZAftBef_vs_f.png")
